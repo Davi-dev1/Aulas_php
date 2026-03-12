2e5f6ee3139fd523b5b77e0cfa2f274bbef8cafb
Davi_dev1 <davigancalveesdasilva@gmail.com>
2026-03-12 13:27:58 +0000

    envinado algumas alteracoes no github

---
+++ src/pg02_1.php
@@ -0,0 +1,19 @@
+<h1>Pg02_1 -12/03/26</h1>
+<?php
+$idade=34;
+$nome="Lukin";
+$entrada=false;
+echo "Idade é igual $idade anos <br> e o  nome do maluco é $nome .";
+$cep = "010121123"; //todo número que começa com zero deve ser string, porque ele vai se trasnformar automaticamente em número octal com base 10  o padrão do octal é 010 p ophp automaticamente vai seta-lo como numero inteiro por isso ele deve ser texto
+echo "<br>cep = $cep <br>";
+
+/*
+bases :
+decimal 0123456789 10
+binaria 01 10
+octal 01234567 10
+hexadecimal 0123456789abcdef 10(10 equivale a 16 em número decimal)
+
+*/
+
+?>

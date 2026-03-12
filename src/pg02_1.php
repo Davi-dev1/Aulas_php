11b1a6e20167222518029b9f1f4e41bca22d39ef
Davi_dev1 <davigancalveesdasilva@gmail.com>
2026-03-12 14:32:35 +0000

    enviando arquivos da materia

--- src/pg02_1.php
+++ src/pg02_1.php
@@ -15,5 +15,24 @@ octal 01234567 10
 hexadecimal 0123456789abcdef 10(10 equivale a 16 em número decimal)
 
 */
+$preco = 10025247.543;
+
+
+echo "preço = R$ ";
+echo number_format($preco,2,",","."); //formatação de números com 2 casas decimais( a primeira virgula serve para separar os dois números finais no caso 123,43(43) os pontos para separar os vlaores)
+$cor ="red";
+echo "<br>cor = $cor ";
+echo 'cor = $cor<br>';
+echo "<h2 style='color : $cor;'>Outro Titulo estilizado com o php</h2>";
+$ativo =false;
+echo "está ativo ? $ativo <br>";
+echo "hr";
+echo "aquela era: ".$cor;
+echo "<hr>";
+$nota = 7.8;
+echo "nota = ".$nota- (int)$nota;//(valor flutuante 7.8 - o valor inteiro formatado )
+
+//arrendodamento deve-se usar o number format, agr se quiser só mostrar os inteiros é só setar o (int) desprezando os números decimais
 
 ?>
+<h2 style='color : blue;'>Outro Titulo 1 </h2>
